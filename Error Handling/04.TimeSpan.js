9385419d6fadab845fb00bb33117b5684d641d5e
pkindalov <panayotkindalov@gmail.com>
2016-11-09 10:38:46 +0200

    Added task Timespan from Error handling lesson

---
+++ Error Handling/04.TimeSpan.js
@@ -0,0 +1,32 @@
+/**
+ * Created by r3v3nan7 on 08.11.16.
+ */
+
+class Timespan{
+    constructor(hours, minutes, seconds){
+        if(!Number.isInteger(hours)){
+            throw new RangeError("Invalid hours: " + hours);
+        }
+        if(!Number.isInteger(minutes)){
+            throw new RangeError("Invalid minutes: " + minutes);
+        }
+        if(!Number.isInteger(seconds)){
+            throw new RangeError("Invalid seconds: " + seconds);
+        }
+
+        this._seconds = seconds + 60 * minutes + 60 * 60 * hours;
+    }
+
+
+    toString(){
+        let seconds = this._seconds % 60;
+        let minutes = Math.trunc(this._seconds / 60) % 60;
+        let hours = Math.trunc(this._seconds / (60 * 60));
+
+        seconds = ("0" + seconds).slice(-2);
+        minutes = ("0" + minutes).slice(-2);
+        return `${hours}:${minutes}:${seconds}`;
+
+    }
+
+}
