030e52bf646c26666c8390b3585b3aa722a81075
pkindalov <panayotkindalov@gmail.com>
2016-11-12 18:03:19 +0200

    Edited Tickets.js

--- Classes and class members/Tickets.js
+++ Classes and class members/Tickets.js
@@ -40,6 +40,42 @@ function solve(arr, sorter) {
 
 }
 
+//Second way
+// function solve(arr, sorter) {
+//     class Ticket{
+//         constructor(destination, price, status){
+//             this.destination = destination;
+//             this.price = Number(price);
+//             this.status = status;
+//         }
+//
+//     }
+//
+//     let result = [];
+//
+//     for(let info of arr){
+//         let infoArr = info.split("|");
+//         let destination = infoArr[0];
+//         let price = Number(infoArr[1]);
+//         let status = infoArr[2];
+//         let ticket = new Ticket(destination, price, status);
+//         result.push(ticket);
+//     }
+//
+//
+//     return result.sort((a, b) => {
+//         if(typeof(a[sorter]) == 'string'){
+//             if(a[sorter] != b[sorter]){
+//                 return a[sorter].localeCompare(b[sorter]);
+//             }
+//         }else {
+//             return a[sorter] - b[sorter];
+//         }
+//     });
+//
+//
+// }
+
 
 
 
