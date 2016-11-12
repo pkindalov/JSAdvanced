095b1fa26e79c910d33729fb8d0671dbe8da77dc
pkindalov <panayotkindalov@gmail.com>
2016-11-12 18:33:11 +0200

    Added exercis Charlookup from Unit testing mocha

---
+++ Unit Testing Mocha/MochaTesting/CharLookup.js
@@ -0,0 +1,17 @@
+/**
+ * Created by r3v3nan7 on 11.11.16.
+ */
+
+function lookupChar(string, index) {
+    if (typeof(string) !== 'string' || !Number.isInteger(index)) {
+        return undefined;
+    }
+    if (string.length <= index || index < 0) {
+        return "Incorrect index";
+    }
+
+    return string.charAt(index);
+}
+
+
+module.exports = {lookupChar};
