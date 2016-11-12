c1b30e09cca704dac84a045cfe9d00d0f9745a19
pkindalov <panayotkindalov@gmail.com>
2016-11-12 18:06:57 +0200

    Updated exercise Unity from Classes and class members

--- Classes and class members/Unity.js
+++ Classes and class members/Unity.js
@@ -1,37 +1,30 @@
-/**
- * Created by r3v3nan7 on 28.10.16.
- */
-
-
-function solve() {
-
-    class Rat{
-        constructor(name){
-            this.name = name;
-            this.united = [];
-        }
-
-
-
-        unite(otherRat){
-             if(otherRat.constructor.name == 'Rat'){
-                 this.united.push(otherRat);
-             }
-        };
-
-
-        getRats(){
-            return this.united;
-
-        };
-
-
-
-
-
-
-
+class Rat{
+    constructor(name){
+        this.name = name;
+        this.united = [];
     }
 
+
+    unite(otherRat){
+        if(otherRat.constructor.name == "Rat"){
+            this.united.push(otherRat);
+        }
+    }
+
+
+    getRats(){
+        return this.united;
+    }
+
+
+    toString(){
+        let result = this.name;
+        for(let rat of this.united){
+            //result += `\###${rat.name}`;
+            result += "###" + rat.name;
+        }
+
+        return result;
+    }
 
 }
